a585f7561490b12fba2d59b58599e2f9b1dc8609
MrGrinch <jamesbernardjudd@gmail.com>
2020-07-19 01:24:42 +1000

    first commit

---
+++ helloworld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string t = Console.ReadLine();
+            Console.WriteLine("Hi, " + t + 2);
+
+            int numb = 5;
+
+            Console.WriteLine("Hi, " + (numb + 2));
+
+            if (numb == 5)
+            {
+                Console.WriteLine("Number is 5");
+            }
+
+            else if (numb > 5)
+            {
+                Console.WriteLine("Number is GT 5");
+
+            }
+            else
+            {
+                Console.WriteLine("Number is NOT 5");
+            }
+        }
+    }
+}
